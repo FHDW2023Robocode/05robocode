e2b33fa86705ac8e5807dc06c9b0722d848c63fe
Thomas Ströder <thomas.stroeder@email.de>
2023-06-15 11:29:49 +0100

    automatic update

--- src/main/java/de/fhdw/robocode/FirstRobot05.java
+++ src/main/java/de/fhdw/robocode/FirstRobot05.java
@@ -4,7 +4,7 @@ import robocode.HitByBulletEvent;
 import robocode.Robot;
 import robocode.ScannedRobotEvent;
 
-public class FirstRobot extends Robot {
+public class FirstRobot05 extends Robot {
 
     @Override
     public void run() {
