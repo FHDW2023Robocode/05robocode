81503602517ac792eb58cd368e249addd81648a8
Moritz Hegmann <hegmann.moritz@gmail.com>
2023-06-17 14:42:01 +0200

    Das kann noch nichts

--- src/main/java/de/fhdw/robocode/IchKlappeNichtUndDasNervt.java
+++ src/main/java/de/fhdw/robocode/IchKlappeNichtUndDasNervt.java
@@ -4,28 +4,55 @@ import robocode.HitByBulletEvent;
 import robocode.Robot;
 import robocode.ScannedRobotEvent;
 
-public class FirstRobot05 extends Robot {
+public class IchKlappeNichtUndDasNervt extends Robot {
 
+	
+	
+	double radius = 100.0;
+    double angle = 90.0;
+    double aimTo;
+    Boolean hasSeenEnemy =false;
     @Override
     public void run() {
 
-        double radius = 100.0;
-        double angle = 90.0;
+        
 
         while (true) {
-            ahead(radius);
-            turnLeft(angle);
-            turnGunLeft(angle);
-            fireBullet(getEnergy());
+        	
+        	if(!hasSeenEnemy) {
+        		
+        		ahead(radius);
+        		turnLeft(angle);
+        		//turnGunLeft(angle);
+        		turnRadarLeft(angle);
+        		//fireBullet(getEnergy());
+        	}else {
+        		turnGunLeft(90 - aimTo);
+            	
+            	
+            	turnLeft(90 - aimTo);
+            	ahead(radius);
+        	}
         }
     }
 
     public void onScannedRobot(ScannedRobotEvent e) {
+    	
+        hasSeenEnemy = true;
+        aimTo = e.getBearing();
         fire(1);
+        /*
+    	turnGunLeft(90 - e.getBearing());
+    	fire(0.1);
+    	
+    	turnLeft(90 - e.getBearing());
+    	ahead(radius);
+        */
     }
 
     public void onHitByBullet(HitByBulletEvent e) {
         turnLeft(90 - e.getBearing());
+        
     }
 
 }
